4b22787b93f99a1b98ea465c5b04d0c07e90acf8
tzuyi <a0988426059@gmail.com>
2022-08-12 09:42:59 +0800

    feat postcss-nesting

--- src/components/CodeHeader.vue
+++ src/components/CodeHeader.vue
@@ -1,14 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+</script>
 
 <template>
   <header class="code_header">
-    
+    <div class="code_header_left">
+      {{ 'untitled' }}
+    </div>
+
+    <div class="code_header_right">
+      <button></button>
+      <button>Sign Up</button>
+      <button>Log In</button>
+    </div>
   </header>
 </template>
 
 <style lang="postcss" scoped>
 .code_header {
   @apply
-  h-40
+  h-14
+  p-2
+  flex
+  items-center
+  justify-between
+  bg-black/[0.8];
+  &_left {
+    @apply
+    text-white;
+  }
+  &_right {
+
+  }
 }
 </style>
