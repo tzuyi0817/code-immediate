75603dafffad8c2e6402b2405841c47e7d111eeb
tzuyi <a0988426059@gmail.com>
2022-08-31 12:07:22 +0800

    test monaco-editor

--- src/components/CodeWrap.vue
+++ src/components/CodeWrap.vue
@@ -1,8 +1,39 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { editor } from 'monaco-editor/esm/vs/editor/editor.api';
+
+const codeEditor = ref();
+
+function createEditor() {
+  const monacoEditor = editor.create(codeEditor.value, {
+    // model: null,
+    minimap: {
+      enabled: false,
+    },
+    wordWrap: 'on',
+    theme: 'vs-dark',
+    // fontSize: 16,
+    fontFamily: 'MonoLisa, monospace',
+    contextmenu: false,
+    fixedOverflowWidgets: true,
+	  language: 'javascript',
+  });
+
+  monacoEditor.onDidChangeModelContent(() => {
+    console.log('onDidChangeModelContent');
+  });
+
+  monacoEditor.onDidBlurEditorText(() => {
+    console.log('onDidBlurEditorText');
+  });
+}
+
+onMounted(createEditor);
+</script>
 
 <template>
   <div class="code_wrap">
-
+    <div ref="codeEditor" class="w-full h-full"></div>
   </div>
 </template>
 
