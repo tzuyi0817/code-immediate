a1ddf0cf311e1adaf7d41406541dcb49128e2428
tzuyi <a0988426059@gmail.com>
2022-09-13 14:49:03 +0800

    test build

--- vite.config.ts
+++ vite.config.ts
@@ -13,15 +13,6 @@ export default defineConfig({
     //   languageWorkers: ['editorWorkerService', 'css', 'html', 'json', 'typescript'],
     // }),
   ],
-  // optimizeDeps: {
-  //   include: [
-  //     `${prefix}/language/json/json.worker`,
-  //     `${prefix}/language/css/css.worker`,
-  //     `${prefix}/language/html/html.worker`,
-  //     `${prefix}/language/typescript/ts.worker`,
-  //     `${prefix}/editor/editor.worker`
-  //   ],
-  // },
   server: {
     port: 8080,
   },
@@ -34,11 +25,11 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          jsonWorker: [`${prefix}/language/json/json.worker`],
-          cssWorker: [`${prefix}/language/css/css.worker`],
-          htmlWorker: [`${prefix}/language/html/html.worker`],
-          tsWorker: [`${prefix}/language/typescript/ts.worker`],
-          editorWorker: [`${prefix}/editor/editor.worker`],
+          JsonWorker: [`${prefix}/language/json/json.worker`],
+          CssWorker: [`${prefix}/language/css/css.worker`],
+          HtmlWorker: [`${prefix}/language/html/html.worker`],
+          TsWorker: [`${prefix}/language/typescript/ts.worker`],
+          EditorWorker: [`${prefix}/editor/editor.worker`],
         },
       },
     },
