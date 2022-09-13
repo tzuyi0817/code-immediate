330ab10a776939b4b244589e3615d9cce88c0584
tzuyi <a0988426059@gmail.com>
2022-09-13 17:53:50 +0800

    test  fix Unexpected usage

--- vite.config.ts
+++ vite.config.ts
@@ -13,15 +13,15 @@ export default defineConfig({
     //   languageWorkers: ['editorWorkerService', 'css', 'html', 'json', 'typescript'],
     // }),
   ],
-  // optimizeDeps: {
-  //   include: [
-  //     `${prefix}/language/json/json.worker`,
-  //     `${prefix}/language/css/css.worker`,
-  //     `${prefix}/language/html/html.worker`,
-  //     `${prefix}/language/typescript/ts.worker`,
-  //     `${prefix}/editor/editor.worker`
-  //   ],
-  // },
+  optimizeDeps: {
+    include: [
+      `${prefix}/language/json/json.worker`,
+      `${prefix}/language/css/css.worker`,
+      `${prefix}/language/html/html.worker`,
+      `${prefix}/language/typescript/ts.worker`,
+      `${prefix}/editor/editor.worker`
+    ],
+  },
   server: {
     port: 8080,
   },
@@ -30,17 +30,17 @@ export default defineConfig({
       "@": resolve(__dirname, "src"),
     },
   },
-  // build: {
-  //   rollupOptions: {
-  //     output: {
-  //       manualChunks: {
-  //         jsonWorker: [`${prefix}/language/json/json.worker`],
-  //         cssWorker: [`${prefix}/language/css/css.worker`],
-  //         htmlWorker: [`${prefix}/language/html/html.worker`],
-  //         tsWorker: [`${prefix}/language/typescript/ts.worker`],
-  //         editorWorker: [`${prefix}/editor/editor.worker`],
-  //       },
-  //     },
-  //   },
-  // },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          jsonWorker: [`${prefix}/language/json/json.worker`],
+          cssWorker: [`${prefix}/language/css/css.worker`],
+          htmlWorker: [`${prefix}/language/html/html.worker`],
+          tsWorker: [`${prefix}/language/typescript/ts.worker`],
+          editorWorker: [`${prefix}/editor/editor.worker`],
+        },
+      },
+    },
+  },
 })
