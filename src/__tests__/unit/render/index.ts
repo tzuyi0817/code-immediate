44a4d779bd839630bbd01a68ec4f628e4b212cc3
tzuyi <a0988426059@gmail.com>
2023-08-01 15:17:50 +0800

    fix: RenderOptions type

--- src/__tests__/unit/render/index.ts
+++ src/__tests__/unit/render/index.ts
@@ -1,14 +1,19 @@
 import type { Component } from 'vue';
-import { render, RenderOptions } from '@testing-library/vue';
+import { render } from '@testing-library/vue';
+import type { RenderOptions } from '@testing-library/vue/types';
 import { setActivePinia, createPinia } from 'pinia';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import LoadingButton from '@/components/LoadingButton.vue';
+
+interface RenderComponentOptions extends RenderOptions {
+  provide?: Record<any, any>;
+}
 
 const pinia = createPinia();
 
 setActivePinia(pinia);
 
-export function renderComponent(testComponent: Component, options?: RenderOptions) {
+export function renderComponent(testComponent: Component, options?: RenderComponentOptions) {
   const componentOptions = options ?? {};
 
   return render(testComponent, {
