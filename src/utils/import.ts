749017476e3e87886bb4de302010ad22fdd1299d
tzuyi <a0988426059@gmail.com>
2024-03-11 17:13:04 +0800

    fix: dynamic import logic

--- src/utils/import.ts
+++ src/utils/import.ts
@@ -1,18 +1,24 @@
 import type { CompileParams, CodeContent } from '@/types/codeContent';
 
-type CompileSfcModule = Record<'compileSfc', (content: CompileParams) => Promise<CodeContent>>;
+type Modules = keyof typeof modules;
 
-const compileSfc = import.meta.glob<CompileSfcModule>('@/utils/compileSfc.ts', { query: '?url', import: 'default' });
-const modules = {
-  compileSfc: transformModule(compileSfc),
-} as const;
-
-function transformModule<T>(module: Record<string, () => Promise<T>>) {
-  const url = Object.keys(module)[0];
-
-  return module[url];
+interface ModulesMap {
+  compileSfc: Record<'compileSfc', (content: CompileParams) => Promise<CodeContent>>;
 }
 
-export function dynamicImport(module: keyof typeof modules) {
-  return modules[module]();
+const compileSfc = import.meta.glob('@/utils/compileSfc.ts', { query: '?url', import: 'default' });
+const modules = {
+  compileSfc: getModule(compileSfc),
+} as const;
+
+async function getModule(module: Record<string, () => Promise<unknown>>) {
+  const key = Object.keys(module)[0];
+
+  return (await module[key]()) as string;
+}
+
+export async function dynamicImport(module: Modules): Promise<ModulesMap[Modules]> {
+  const url = await modules[module];
+
+  return import(url);
 }
