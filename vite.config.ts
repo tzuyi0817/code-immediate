d4288c4b7727e74837e3dcf7901d64468df9f25a
tzuyi <a0988426059@gmail.com>
2024-03-19 18:02:14 +0800

    chore: build test

--- vite.config.ts
+++ vite.config.ts
@@ -74,7 +74,6 @@ export default defineConfig({
         chunkFileNames: 'chunks/[name]-[hash].js',
         manualChunks: filepath => {
           if (filepath.includes('compiler-sfc.esm-browser')) return 'compiler-sfc-esm-browser';
-          if (filepath.includes('monaco-textmate')) return 'monaco-textmate';
           if (filepath.includes('@fortawesome')) return '@fortawesome';
         },
       },
