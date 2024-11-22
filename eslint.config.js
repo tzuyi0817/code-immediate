b9f13bed15d6c4422140bf37ca82b0dbc73ec7f9
tzuyi <a0988426059@gmail.com>
2024-11-22 18:15:33 +0800

    feat: adjust markdown eslint order of use

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,10 @@ import componentHookPlugin from '@component-hook/eslint-plugin';
 export default [
   ...componentHookPlugin.configs.basic,
   ...componentHookPlugin.configs.vue,
-  ...componentHookPlugin.configs.markdown,
   componentHookPlugin.configs.prettier,
   ...componentHookPlugin.configs.sonarjs,
   componentHookPlugin.configs.security,
+  ...componentHookPlugin.configs.markdown,
   {
     files: ['**/__tests__/unit/**/*.test.[jt]s?(x)'],
     ...componentHookPlugin.configs['testing-library/vue'],
