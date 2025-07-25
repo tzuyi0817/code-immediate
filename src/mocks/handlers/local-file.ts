ddb6294ca9900761564b3000384f6d76c6541a75
tzuyi <a0988426059@gmail.com>
2025-07-25 11:50:09 +0800

    test: fix json file path error

--- src/mocks/handlers/local-file.ts
+++ src/mocks/handlers/local-file.ts
@@ -3,20 +3,20 @@ import { http, HttpResponse } from 'msw';
 export const mockLocalFiles = [
   http.get('*/grammars/html.tmLanguage.json', async () => {
     const { fileURLToPath, URL } = await import('node:url');
-    const jsonFile = await import(fileURLToPath(new URL('public/grammars/html.tmLanguage.json', import.meta.url)));
+    const jsonFile = await import(fileURLToPath(new URL('/public/grammars/html.tmLanguage.json', import.meta.url)));
 
     return HttpResponse.json(jsonFile);
   }),
   http.get('*/grammars/css.tmLanguage.json', async () => {
     const { fileURLToPath, URL } = await import('node:url');
-    const jsonFile = await import(fileURLToPath(new URL('public/grammars/css.tmLanguage.json', import.meta.url)));
+    const jsonFile = await import(fileURLToPath(new URL('/public/grammars/css.tmLanguage.json', import.meta.url)));
 
     return HttpResponse.json(jsonFile);
   }),
   http.get('*/grammars/javascript.tmLanguage.json', async () => {
     const { fileURLToPath, URL } = await import('node:url');
     const jsonFile = await import(
-      fileURLToPath(new URL('public/grammars/javascript.tmLanguage.json', import.meta.url))
+      fileURLToPath(new URL('/public/grammars/javascript.tmLanguage.json', import.meta.url))
     );
 
     return HttpResponse.json(jsonFile);
